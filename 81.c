79dca3b99fb755ee88949cc4ad9efb3fec0b1f1a
Akashjindall <177572309+Akashjindall@users.noreply.github.com>
2026-09-15 15:52:31 +0530

    Add files via upload

---
+++ 81.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Bubble Sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    // Print sorted array
+    for (i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1)
+            printf(" ");
+    }
+    
+    return 0;
+}
